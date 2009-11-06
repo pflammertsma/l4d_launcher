d8e52262b524370da27934bec98dccec08eedd8f
paul <paul@63d26091-f8e8-4879-aa43-80173e6a04d1>
2009-11-06 16:02:20 +0000

    Fixed 'not found' dialog exiting application

--- Left 4 Dead launcher/NotFound.vb
+++ Left 4 Dead launcher/NotFound.vb
@@ -2,10 +2,6 @@
 
 Public Class NotFound
 
-    Private Sub NotFound_FormClosed(ByVal sender As Object, ByVal e As System.Windows.Forms.FormClosedEventArgs) Handles Me.FormClosed
-        Me.DialogResult = System.Windows.Forms.DialogResult.Cancel
-    End Sub
-
     Private Sub Dialog1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
         lblInfo.Text = "Left 4 Dead could not be located via the Windows Uninstall list. This means that not only can you not use this Launcher, but you are also unlikely to be able to uninstall the game. This can automatically be fixed by entering the path to the game below."
         If Not VistaSecurity.IsAdmin Then
@@ -25,18 +21,18 @@ Public Class NotFound
     End Function
 
     Private Sub btnFix_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnFix.Click
-        Me.Close()
         Me.DialogResult = System.Windows.Forms.DialogResult.Retry
+        Me.Close()
     End Sub
 
     Private Sub btnContinue_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnContinue.Click
-        Me.Close()
         Me.DialogResult = System.Windows.Forms.DialogResult.OK
+        Me.Close()
     End Sub
 
     Private Sub btnExit_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnExit.Click
-        Me.Close()
         Me.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.Close()
     End Sub
 
 End Class
